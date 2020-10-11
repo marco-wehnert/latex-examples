9bbd1efd5f3580868c9c47723f0f1fefcc2af42a
Marco Wehnert <marco.wehnert@gmx.de>
2020-10-12 00:39:23 +0200

    Added clean.sh and insert bibliography into document.tex.

---
+++ document/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+/bin/rm *.aux 2>/dev/null
+/bin/rm *.bbl 2>/dev/null
+/bin/rm *.blg 2>/dev/null
+/bin/rm *.log 2>/dev/null
+/bin/rm *.out 2>/dev/null
+/bin/rm *.pdf 2>/dev/null
+/bin/rm *.toc 2>/dev/null
+
+
---
+++ document/listing.java
@@ -0,0 +1,10 @@
+// Crceate line until column 78
+//----------------------------------------------------------------------------
+ package test;
+
+ public class Example {
+
+     public static void Test() {
+
+     }
+ }
